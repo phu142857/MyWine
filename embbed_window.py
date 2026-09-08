7588bc6a184b7e112ab51bc46a727c0f6d5b492c
phu142857 <77288224+phu142857@users.noreply.github.com>
2026-09-08 09:44:28 +0700

    Add EmbeddedWindow class with aspect ratio property

--- embbed_window.py
+++ embbed_window.py
@@ -1,1 +1,16 @@
+from dataclasses import dataclass
 
+@dataclass
+class EmbeddedWindow:
+    win_id: int
+    native_width: int = 0
+    native_height: int = 0
+
+    @property
+    def aspect_ratio(self) -> float:
+        if self.native_height == 0:
+            return 1.0
+
+        return self.native_width / self.native_height
+
+        
